7a5e5e675d874d02be564ec04e01aefb44429e19
Dylan Agiman <dylanagiman@gmail.com>
2018-09-27 13:07:14 -0700

    finished the printing of data+cos plus initial formatting

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import urllib.request
+import math
 
 link = "http://rapid-hub.org/data/angles_UCI_CS.csv"
 file = urllib.request.urlopen(link).read().decode("utf-8")
 
-# splitting the input file, ignoring the first comment
-lines = file.split("\r\n")
+# splitting the input file, ignoring the last empty line
+lines = file.split("\r\n")[:-1]
 
-print("\n\nprinting lines after splitting\n")
-for i, line in enumerate(lines):
-    print(i, ": ",line)
+
+#for debugging purposes to check the contents of the csv
+#print("\n\nprinting lines after splitting\n")
+#for i, line in enumerate(lines):
+#    print(i, ": ",line)
 
 # tbd: manage the formatting of the output and calculate the cosine
+print("station_id  angle_degrees  cos_values")
+for i, line in enumerate(lines[1:]):
+    elems = [elem.strip() for elem in line.split(',')]
+    station_id = int(elems[0])
+    angle = float(elems[1])
+    print("{:10d}  {:13}  {:10f}".format(station_id, angle, math.cos(math.radians(angle))))
